c1c3e6ca656d4e9aa82336f5aa548f4483ced635
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-10-28 08:11:43 +0700

    fix: index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
 <?php 
-require_once 'view/components/head.php';
+   require_once 'view/components/head.php';
 ?>
-
+</head>
 <body>
    <?php 
    require_once 'view/todo.php'; 
